4a4f54d52e03f156f2aa22c75a4b1e9b510a1779
Stephen Finucane <stephen@that.guru>
2020-03-07 13:18:52 +0000

    Switch to oslotest
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,10 +1,7 @@
-import unittest
+from oslotest import base
 
 
-class TestUM(unittest.TestCase):
-
-    def setUp(self):
-        pass
+class TestUM(base.BaseTestCase):
 
     def test_numbers_3_4(self):
         self.assertEqual(12, 3 * 4)
